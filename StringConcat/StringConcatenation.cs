1d6713909cc819036ae0d1f8cfdd9f03fa9b66d0
Adam Carpenter <carpenteral91@gmail.com>
2019-05-13 15:29:51 +0100

    completed tests

--- StringConcat/StringConcatenation.cs
+++ StringConcat/StringConcatenation.cs
@@ -6,7 +6,15 @@ namespace StringConcat
     {
         public string addStrings(string lh, string rh)
         {
-            return lh + " " + rh;
+            if(lh.Equals(" "))
+            {
+                return lh + " " + rh.Trim();           
+            } else if (rh.Equals(" "))
+            {
+                return lh.Trim() + " " + rh;
+            }
+
+            return lh.Trim() + " " + rh.Trim();
         }
     }
 }
--- StringConcatenation/StringConcatenationTest.cs
+++ StringConcatenation/StringConcatenationTest.cs
@@ -21,5 +21,125 @@ namespace StringConcatenationTestModule
             // Assert
             Assert.AreEqual(expected, result);
         }
+        [TestMethod]
+        public void test_add_lh_space_rh_no_space_result_lh_space_rh()
+        {
+            // Arrange
+            StringConcatenation cut = new StringConcatenation();
+            string lh = "AA ";
+            string rh = "BB";
+            string expected = "AA BB";
+
+            // Act
+            string result = cut.addStrings(lh, rh);
+
+            // Assert
+            Assert.AreEqual(expected, result);
+        }
+        [TestMethod]
+        public void test_add_lh_no_space_rh_space_result_lh_space_rh()
+        {
+            // Arrange
+            StringConcatenation cut = new StringConcatenation();
+            string lh = "AA";
+            string rh = "BB ";
+            string expected = "AA BB";
+
+            // Act
+            string result = cut.addStrings(lh, rh);
+
+            // Assert
+            Assert.AreEqual(expected, result);
+        }
+        [TestMethod]
+        public void test_add_space_lh_rh_no_space_result_lh_space_rh()
+        {
+            // Arrange
+            StringConcatenation cut = new StringConcatenation();
+            string lh = " AA";
+            string rh = "BB";
+            string expected = "AA BB";
+
+            // Act
+            string result = cut.addStrings(lh, rh);
+
+            // Assert
+            Assert.AreEqual(expected, result);
+        }
+        [TestMethod]
+        public void test_add_lh_no_space_space_rh_result_lh_space_rh()
+        {
+            // Arrange
+            StringConcatenation cut = new StringConcatenation();
+            string lh = "AA";
+            string rh = " BB";
+            string expected = "AA BB";
+
+            // Act
+            string result = cut.addStrings(lh, rh);
+
+            // Assert
+            Assert.AreEqual(expected, result);
+        }
+        [TestMethod]
+        public void test_add_lh_space_rh_space_result_lh_space_rh()
+        {
+            // Arrange
+            StringConcatenation cut = new StringConcatenation();
+            string lh = "AA";
+            string rh = " BB ";
+            string expected = "AA BB";
+
+            // Act
+            string result = cut.addStrings(lh, rh);
+
+            // Assert
+            Assert.AreEqual(expected, result);
+        }
+        [TestMethod]
+        public void test_add_lh_blank_rh_blank_result_space()
+        {
+            // Arrange
+            StringConcatenation cut = new StringConcatenation();
+            string lh = "";
+            string rh = "";
+            string expected = " ";
+
+            // Act
+            string result = cut.addStrings(lh, rh);
+
+            // Assert
+            Assert.AreEqual(expected, result);
+        }
+        [TestMethod]
+        public void test_add_lh_blank_space_rh_blank_result_space()
+        {
+            // Arrange
+            StringConcatenation cut = new StringConcatenation();
+            string lh = " ";
+            string rh = "";
+            string expected = "  ";
+
+            // Act
+            string result = cut.addStrings(lh, rh);
+
+            // Assert
+            Assert.AreEqual(expected, result);
+        }
+        [TestMethod]
+        public void test_add_lh_blank_space_rh_blank_space_result_space()
+        {
+            // Arrange
+            StringConcatenation cut = new StringConcatenation();
+            string lh = "";
+            string rh = " ";
+            string expected = "  ";
+
+            // Act
+            string result = cut.addStrings(lh, rh);
+
+            // Assert
+            Assert.AreEqual(expected, result);
+        }
     }
 }
